2b4024365315bcdb2c56d1acdf4ac6a2819348af
Minh Thẩn <66764036+thanethereal@users.noreply.github.com>
2024-04-24 18:43:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host=0.0.0.0,port=5000)
+    app.run(debug=True,host="0.0.0.0",port=5000)
